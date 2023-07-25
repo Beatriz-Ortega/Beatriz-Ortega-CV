23eaee652d802f0167fa443b1176e6af9a8e35ca
Beatriz Ortega <ortega.bea87@gmail.com>
2023-07-25 10:46:09 -0400

    first commit

---
+++ src/components/SectccionSobreMi.jsx
@@ -0,0 +1,18 @@
+
+export const SeccionSobreMi = () => {
+  return (
+    <section className="container text-center sobre_mi">
+        <div className='row justify-content-center'>
+            <div className='col-12 fs-5 mt-5'>
+                <h2 className='fw-bold text-start mt-5 sobre_mi_text text-body-tertiary d-flex'>
+                  <p className='text-nombre-color'>S</p>obre mi</h2>
+                <p className='mt-4 text-start'>
+                  Ingeniera de Sistemas con experiencia en desarrollo web, programación orientada a objetos y estructuras de datos. Mi enfoque principal se centra en el diseño y la implementación de soluciones web interactivas y de alto rendimiento, asegurando que los sitios web sean accesibles, receptivos y visualmente atractivos en diferentes dispositivos y navegadores. <br/><br/>
+                  Tengo experiencia en el uso de tecnologías front-end como HTML, CSS y JavaScript, así como en el uso de frameworks y bibliotecas como React y Vue.js. Además, tengo habilidades sólidas en la optimización del rendimiento web y la resolución de problemas relacionados con la compatibilidad entre navegadores. <br/><br/>
+                  Poseo una actitud proactiva y un enfoque orientado a la resolución de problemas. Estoy constantemente aprendiendo y manteniéndome actualizada sobre las últimas tendencias y tecnologías en desarrollo web. Busco oportunidades desafiantes que me permitan crecer profesionalmente y contribuir al éxito de la organización a través de la excelencia técnica y la pasión por el desarrollo.
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
---
+++ src/data/cvData.js
@@ -0,0 +1,253 @@
+export const educationData = [
+    {
+        course: 'Ingeniero de Sistemas',
+        institute: 'Universidad de Los Andes. Mérida, Venezuela.',
+        date: 'Octubre 2014'
+    },
+    {
+        course: 'Vue.Js de cero a experto',
+        institute: 'Udemy.',
+        date: 'Julio 2023'
+    },
+    {
+        course: 'React la guía completa Hooks context Redux Mern',
+        institute: 'Udemy',
+        date: 'Julio 2023'
+    },
+    {
+        course: 'Java Workshop',
+        institute: 'ModMed',
+        date: 'Junio 2023'
+    },
+    {
+        course: 'Bootstrap 5 y Sass',
+        institute: 'Udemy.',
+        date: 'Junio 2023'
+    },
+    {
+        course: 'The complete SASS And SCSS cource',
+        institute: 'Udemy.',
+        date: 'Abril 2023'
+    },
+    {
+        course: 'Selenium testing framework con python',
+        institute: 'Udemy.',
+        date: 'Enero 2023'
+    },
+    {
+        course: 'Fundamentos de pruebas de software - QA',
+        institute: 'Udemy.',
+        date: 'Enero 2023'
+    },
+    {
+        course: 'Universidad desarrollo web - FrontEnd Web Developer',
+        institute: 'Udemy',
+        date: 'Noviembre 2022'
+    },
+    {
+        course: 'Product Owner Certification',
+        institute: 'Scrum Manager',
+        date: 'Agosto 2022'
+    },
+    {
+        course: 'Web Development fundamentals, JavaScript, React + Redux, SQL',
+        institute: 'Solo Learn.',
+        date: 'Agosto 2022'
+    }
+]
+
+export const habilitysData = [
+    {
+        title: 'Vue.Js',
+        description: 'Vue.Js',
+        icons: 'fa-brands fa-vuejs fa-2x'
+    },
+    {
+        title: 'JavaScript',
+        description: 'JavaScript',
+        icons: 'fa-brands fa-square-js fa-2x'
+    },
+    {
+        title: 'Sass',
+        description: 'Sass',
+        icons: 'fa-brands fa-sass fa-2x'
+    },
+    {
+        title: 'Boostrap',
+        description: 'Boostrap',
+        icons: 'fa-brands fa-bootstrap fa-2x'
+    },
+    {
+        title: 'HTML',
+        description: 'HTML',
+        icons: 'fa-brands fa-html5 fa-2x'
+    },
+    {
+        title: 'CSS',
+        description: 'CSS',
+        icons: 'fa-brands fa-css3-alt fa-2x'
+    },
+    {
+        title: 'Node JS',
+        description: 'Node JS',
+        icons: 'fa-brands fa-node fa-2x'
+    },
+    {
+        title: 'React',
+        description: 'React',
+        icons: 'fa-brands fa-react fa-2x'
+    },
+    {
+        title: 'GIT',
+        description: 'GIT',
+        icons: 'fa-brands fa-github fa-2x'
+    },
+    {
+        title: 'Bases de datos',
+        description: 'MongoDB',
+        icons: 'fa-solid fa-database fa-2x'
+    },
+    {
+        title: 'Jira',
+        description: 'Jira',
+        icons: 'fab fa-jira fa-2x'
+    }
+
+]
+
+export const experienceData = [
+    {
+        title: 'Desarrolladora Front End',
+        charge: 'Digital Beat SPA',
+        date: 'Marzo 2023 - Junio 2023',
+        description: `Desarrollo de web components según requerimiento del área de diseño utilizando Bootstrap, Sass, CSS, JavaScript 
+        con utilidades dinámicas e interactivas. Desarrollo de documentación para los componente elaborados.`
+    },
+    {
+        title: 'Desarrolladora Web Freelance',
+        charge: 'Freelance',
+        date: 'Septiembre 2022 - Marzo 2023',
+        description: `Elaborar paginas y aplicaciones web funcionales utilizando herramientas de desarrollo web como: HTML, React, 
+        Node.js, Express y MongoDB (Stack MERN), CSS, JavaScript, entre otros. Conocimientos en el sistema de versionado GIT.`
+    },
+    {
+        title: 'Cofundadora',
+        charge: 'Fruitastyc SPA',
+        date: 'Junio 2018 - Septiembre 2022',
+        description: `Levantar requerimientos con clientes y crear propuestas comerciales utilizando un enfoque de ventas consultivo 
+        para maximizar los resultados del cliente. Desarrollar y gestionar con éxito nuevos canales de venta digitales, ecommerce y 
+        marketplace.`
+    },
+    {
+        title: 'Encargada de calidad y control de gestión',
+        charge: 'Ingeniería Eléctrica LTDA',
+        date: 'MArzo 2016 - Junio 2018',
+        description: `Responsable de generar Kpi's que permitieran analizar el nivel de desempeño en las diferentes áreas de la empresa 
+        y efectuar propuestas de mejora. Realizar el levantamiento de procesos de las diferentes áreas y elaborar su respectivo 
+        procedimiento. Generar manuales de procedimiento y gestión de calidad, según la normativa ISO 9001:2015.`
+    }
+]
+
+export const projectsData = [
+    {
+        title: 'Calculadora',
+        description: 'Calculadora creada con HTML, CSS y JS',
+        url: 'https://github.com/Beatriz-Ortega/Calculadora',
+        img: '../../public/img/web.jpg',
+        alt: 'calculadora',
+        ref: 'https://github.com/Beatriz-Ortega/Calculadora',
+        tech:{
+            javascript:true,
+            css:true,
+            html:true
+        }
+    },
+    {
+        title: 'Cotizar Cripto',
+        description: `Cotizador de criptomonedas con API Rest`,
+        url: 'https://github.com/vortegar/Journal-App',
+        img: '../../public/img/web2.jpg',
+        alt: 'Cotizador Cripto',
+        ref: 'https://github.com/vortegar/Journal-App',
+        tech:{
+            javascript:true,
+            html:true,
+            css:true,
+            react: true
+        }        
+    },
+    {
+        title: 'Carro de compras',
+        description: `Página web con carro de compras`,
+        // React boostrsap
+        url: 'https://luxury-elf-6360d3.netlify.app/',
+        img: '../../public/img/web3.jpg',
+        alt: 'Carro de compras',
+        ref: 'https://luxury-elf-6360d3.netlify.app/',
+        tech:{
+            javascript:true,
+            html:true,
+            css: true
+        }
+    },
+    {
+        title: 'Citas veterinarias',
+        description: `Sistema de citas creado con React`,
+        //creada utilizando HTML, Bootstrap y Sass.
+        url: 'https://graceful-meringue-f4104e.netlify.app/',
+        img: '../../public/img/web.jpg',
+        alt: 'Citas Veterinarias',
+        ref: 'https://graceful-meringue-f4104e.netlify.app/',
+        tech:{
+            react:true,
+            html: true,
+            sass:true
+        }
+    },
+    {
+        title: 'Control de presupuesto',
+        description: `Sistema de control de presupuesto`,
+        url: 'https://mellow-cat-24a356.netlify.app/',
+        img: '../../public/img/web2.jpg',
+        alt: 'Control de presupuesto',
+        ref: 'https://mellow-cat-24a356.netlify.app/',
+        tech:{
+            javascript:true,
+            html:true,
+            css:true,
+            react: true,
+        }        
+    }
+]
+
+export const experienciaData = [
+    {
+        title: 'Desarrolladora Front End',
+        subtitle:'Digital Beat SPA',
+        fecha: 'Marzo 2023 a Junio 2023',
+        description: `Desarrollo de web components según requerimiento del área de diseño utilizando Bootstrap, Sass, CSS, 
+        JavaScript con utilidades dinámicas e interactivas. Desarrollo de documentación para los componente desarrollados`,
+    },
+    {
+        title: 'Desarrolladora Front End',
+        subtitle:'Freelance',
+        fecha: 'Septiembre 2022 a Marzo 2023',
+        description: `laborar paginas y aplicaciones web funcionales utilizando herramientas de desarrollo web como: HTML, React, 
+        Node.js, Express y MongoDB (Stack MERN), CSS, JavaScript, entre otros. Conocimientos en el sistema de versionado GIT.`,
+    },
+    {
+        title: 'Cofundadora y Encargada comercial',
+        subtitle:'Fruitastyc SPA',
+        fecha: 'Junio 2018 a Septiembre 2022',
+        description: `Levantar requerimientos con clientes y crear propuestas comerciales utilizando un enfoque de ventas consultivo para 
+        maximizar los resultados del cliente. Desarrollar y gestionar con éxito nuevos canales de venta digitales, ecommerce y marketplace.`,
+    },
+    {
+        title: 'Encarga de Calidad y Controlde Gestión',
+        subtitle:'Ingeniería Eléctrica Ltda',
+        fecha: 'Marzo 2016 a Junio 2018',
+        description: `Responsable de generar Kpi que permitieran analizar el nivel de desempeño en las diferentes áreas de la empresa y 
+        efectuar propuestas de mejora. Realizar el levantamiento de procesos de las diferentes áreas Generar manuales de procedimiento 
+        y gestión de calidad, según la normativa ISO 9001:2015.`,
+    }
+]
